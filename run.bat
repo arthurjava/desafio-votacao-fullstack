8b05e58620826a440160ada2840c49e2ab4c61be
Arthur Melo <99330896+arthurjava@users.noreply.github.com>
2026-09-04 00:04:53 -0300

    Remove directory change from run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
 @echo off
-cd D:\Projetos\db\desafio-votacao-fullstack
 java -jar target\desafio-votacao-1.0.0.jar --spring.datasource.url=jdbc:postgresql://localhost:5432/votacao --spring.datasource.username=votacao --spring.datasource.password=votacao123 > app.log 2>&1
